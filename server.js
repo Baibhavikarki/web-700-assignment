e46b39c2d1bf90151588145890bed75c9d169ce7
Baibhavikarki <baibhavikarki325@gmail.com>
2022-11-20 16:10:01 -0500

    portfix

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 /********************************************************************************* 
-*  WEB700 – Assignment 04
+* WEB700 – Assignment 05
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part  
 *  of this assignment has been copied manually or electronically from any other source  
 *  (including 3rd party web sites) or distributed to other students. *  
-*  Name:Baibhavi Karki Student ID: 120544226 Date: Nov 04, 2022 
+*  Name:Baibhavi Karki Student ID: 120544226 Date: Nov 20, 2022 
+*  Online (Cyclic) Link: 
 * ********************************************************************************/
 
-var HTTP_PORT = process.env.PORT || 8085;
+var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
 const path = require("path");
